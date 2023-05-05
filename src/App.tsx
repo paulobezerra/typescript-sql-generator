17f6d9693bc7411c5d6b90790dbff88de40e0f1b
Paulo Bezerra <bezerra.paulo@fcamara.com.br>
2023-05-05 09:01:39 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CodeDisplay from "./components/CodeDisplay";
+import MessagesDisplay from "./components/MessagesDisplay";
 
-function App() {
+interface ChatData {
+  role: string,
+  content: string,
+}
+
+const App = () => {
+  const [value, setValue] = useState("")
+  const [chat, setChat] = useState<ChatData[]>([])
+
+  const getQuery = async () => {
+    try {
+      const options: RequestInit = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          message: value,
+        }),
+      };
+
+      const response = await fetch("http://localhost:8000/completions", options);
+      const data = await response.json();
+      console.log(data);      
+
+      const userMessage = {
+        role: "user",
+        content: value
+      }
+
+      setChat(oldChat => [...oldChat, data, userMessage])
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const clearChat = () => {
+    setValue("");
+    setChat([]);
+  }
+
+  const filteredUsersMessages = chat.filter(message => message.role === "user");
+
+  const latestCode = chat.filter(message => message.role === "assistant").pop();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input value={value} onChange={e => setValue(e.target.value)} placeholder="What do you want to do with SQL?"/>
+      <div className="button-container">
+        <button id="get-query" onClick={getQuery}>
+          Get Query!
+        </button>
+        <button id="clear-chat" onClick={clearChat}>Clear Chat</button>
+      </div>
+      <CodeDisplay text={latestCode?.content || ""}/>
+      <MessagesDisplay userMessages={filteredUsersMessages}/>
     </div>
   );
-}
+};
 
 export default App;
